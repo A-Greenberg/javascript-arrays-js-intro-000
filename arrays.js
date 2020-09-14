c2b84cca1eeb743209d6c61ebac6f8a6d7b266a5
Alon G <greenberg.alon@gmail.com>
2020-09-14 23:23:40 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,5 @@ function addElementToBeginningOfArray (array, element) {
 function destructivelyAddElementToBeginningOfArray (array, element){
   newArray = [...array];
   return newArray.unshift(element);
-  
+
 }
-
-
