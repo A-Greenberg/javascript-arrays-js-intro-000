566cf4ea052a2909660c640f10a04b12c9dc9ca5
Alon G <greenberg.alon@gmail.com>
2020-09-14 23:31:31 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -7,3 +7,13 @@ function destructivelyAddElementToBeginningOfArray (array, element){
   array.unshift(element);
   return array;
 }
+
+function addElementToEndOfArray (array, element) {
+  var newArray = [...array];
+  newArray.push(element);
+  return array;
+}
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array;
+}
